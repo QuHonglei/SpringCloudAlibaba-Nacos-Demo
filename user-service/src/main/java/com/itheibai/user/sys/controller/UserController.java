f2a571438a047b81263311c7307c3b20cd05b548
Qu Honglei <948864839@qq.com>
2022-04-22 20:54:03 +0800

    1.nacos实现配置管理

--- user-service/src/main/java/com/itheibai/user/sys/controller/UserController.java
+++ user-service/src/main/java/com/itheibai/user/sys/controller/UserController.java
@@ -1,17 +1,20 @@
 package com.itheibai.user.sys.controller;
 
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.itheibai.user.sys.pojo.User;
 import com.itheibai.user.sys.service.UserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
-import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Locale;
 
 /**
  * <p>
@@ -24,11 +27,31 @@ import java.util.List;
 @Api(tags = "部门控制类") //类名注释
 @Slf4j
 @RestController
+@RefreshScope // 配置nacos中心的配置文件热更新
 @RequestMapping("/sys/user")
 public class UserController {
 
     @Autowired
     UserService userService;
+
+    @Value("${pattern.dateformat:}")  //注入的时候获取不到值，记得检查是否导入了bootstrap的依赖
+    private String dateformat;
+
+    /**
+     * 测试配置中心的文件是否注入进来
+     * 配置中心文件名称为：userService-dev.yaml
+     * 内容：
+     * pattern:
+     *   dateformat: yyyy-MM-dd HH:mm:ss
+     * @return
+     */
+    @RequestMapping("now")
+    @ApiOperation(value = "查询时间信息",response = User.class,httpMethod = "GET") //方法注释
+    public String now(){
+        System.out.println(Locale.CHINA);
+        System.out.println(dateformat);
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern(dateformat, Locale.CHINA));
+    }
 
     /**
      * 根据用户id,查询信息
